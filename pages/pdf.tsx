494bbc33530bb5a9e6622db2b06be2cc10f4688c
walmac <walmac_snake@hotmail.com>
2021-05-26 04:12:14 -0300

    Corrección de cafecito

--- pages/pdf.tsx
+++ pages/pdf.tsx
@@ -209,7 +209,7 @@ const PdfView = (props) => {
                   <Text as={"span"} color={"gray.500"}>
                         Si te gusto la app podés ayudar al desarrollador invitandole un cafecito
                     </Text>
-                  <a href='https://cafecito.app/walmac' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_1.png' alt='Invitame un café en cafecito.app' /></a>
+                  <a href='https://cafecito.app/walmac' rel='noopener' target='_blank'><img srcSet='https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_1.png' alt='Invitame un café en cafecito.app' /></a>
                   </Stack>
                 {/* {!sending ?<Button  type="submit" // Create a submit button
                 disabled={!myForm.isValid}>Envia</Button> : 
